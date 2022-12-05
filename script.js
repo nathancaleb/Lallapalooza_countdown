4499203c805e266ff192e1a7d2462611820ebe25
Nathan Caleb <nathancalebss@gmail.com>
2022-12-05 12:21:09 -0300

    create modal subscribe and thankyou script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* #################### SCRIPT CONTADOR #################### */
 // Variavel recebendo a data/hora final do contador
 const countDownFinal = new Date("nov 20, 2023 20:00:00").getTime();
 
@@ -40,3 +41,174 @@ let x = setInterval(function() {
     }
     
 }, 1000);
+
+/* #################### SCRIPT MODAL #################### */
+// Pegar o modal de inscrição
+const modal = document.getElementById("modal_subscribe");
+
+// Pega o botão de inscrição que ira abrir o modal de inscrição
+const sub_button = document.getElementById("subscribe_button");
+
+// Pega o botão que fecha o modal de inscrição
+const close_modal_button = document.getElementsByClassName("close_modal")[0];
+
+// Pega o botão de envio do modal de inscrição
+const form_send_button = document.getElementById("form_send");
+
+// Pega o modal de thankyou
+const thankyou_modal = document.getElementById("thankyou_box");
+
+// Pega o botão que fecha o modal de agradecimento
+const close_thankyou_button = document.getElementsByClassName("close_modal")[1];
+
+// Quando clicar no botão de subscribe, o modal irá ficar visivel (abrir)
+sub_button.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Quando usuario clicar no (x), o modal deixara de ficar visivel (fechar)
+close_modal_button.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+/* #################### SCRIPT FORMULARIO #################### */
+
+form_send_button.onclick = function(){
+
+    // Atribuindo o value dos inputs para as respectivas variaveis
+    let fname_value = document.getElementById("subscribe_first_name").value;
+    let lname_value = document.getElementById("subscribe_last_name").value;
+    let mail_value = document.getElementById("subscribe_mail").value;
+
+    // Atribuindo o os inputs para as respectivas variaveis
+    const fname_input = document.getElementById("subscribe_first_name");
+    const lname_input = document.getElementById("subscribe_last_name");
+    const mail_input = document.getElementById("subscribe_mail");
+    
+    // Atribuindo o elemento que ira apresentar erro para as respectivas variaveis
+    const message_fname = document.getElementById("erro_message_first_name");
+    const message_lname = document.getElementById("erro_message_last_name");
+    const message_mail = document.getElementById("erro_message_mail");
+    
+    // Variaveis de validação, como todas estão invalidas, recebem false
+    let fname_validation = false;
+    let lname_validation = false;
+    let mail_validation = false;
+
+    // Atribui o patern dos inputs as respectivas variaveis
+    let pattern_fname = fname_input.getAttribute("pattern");
+    let pattern_lname = lname_input.getAttribute("pattern");
+    let pattern_mail = mail_input.getAttribute("pattern");
+    
+    // Função para validar o parttern do primeiro nome
+    function check_pattern_fname(){
+        let teste_fname = new RegExp(pattern_fname);
+        
+        if(teste_fname.test(fname_value)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Função para validar o parttern do ultimo nome
+    function check_pattern_lname(){
+        let teste_lname = new RegExp(pattern_lname);
+        
+        if(teste_lname.test(lname_value)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Função para validar o parttern do email
+    function check_pattern_mail(){
+        let teste_mail = new RegExp(pattern_mail);
+        
+        if(teste_mail.test(mail_value)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Validando se o campo primeiro nome esta digitado corretamente
+    try{
+        if(fname_value == "" || check_pattern_fname() != true) {throw "<b>* Please, type your first name correctly.</b>";}
+        else{
+            fname_validation = true;
+            message_fname.innerHTML = ""
+        }
+    }
+    catch(err_fname){
+        message_fname.innerHTML = err_fname;
+    }
+
+    // Validando se o campo ultimo nome esta digitado corretamente
+    try{
+        if(lname_value == "" || check_pattern_lname() != true) {throw "<b>* Please, type your last name correctly.</b>";}
+        else{
+            lname_validation = true;
+            message_lname.innerHTML = "";
+        }
+    }
+    catch(err_lname){
+        message_lname.innerHTML = err_lname;
+    }
+
+    // Validando se o campo e-mail esta digitado corretamente
+    try{
+        if(mail_value == "" || check_pattern_mail() != true) {throw "<b>* Please, type your e-mail address correctly.</b>";}
+        else{
+            mail_validation = true;
+            message_mail.innerHTML = "";
+        }
+    }
+    catch(err_mail){
+        message_mail.innerHTML = err_mail;
+    }
+
+    // Se as validações estiverem ok, fecha o modal form e abre o modal thankyou
+    if(fname_validation && lname_validation && mail_validation == true){
+            modal.style.display = "none";
+            thankyou_modal.style.display = "block";
+    }
+}
+
+// Quando usuario clicar em qualquer lugar da tela, o modal de thankyou deixara de ficar visivel (fechar)
+
+window.onclick = function(event) {
+    if (event.target == thankyou_box) {
+        // thankyou fica invisivel (fecha)
+        thankyou_modal.style.display = "none";
+      
+       //Declarados para limpar os inputs
+        let fname_input = document.getElementById("subscribe_first_name");
+        let lname_input = document.getElementById("subscribe_last_name");
+        let mail_input = document.getElementById("subscribe_mail");
+        //limpando inputs
+        fname_input.value = '';
+        lname_input.value = '';
+        mail_input.value = '';
+    }
+  }
+
+document.addEventListener('keydown', function(event){
+    // Declarando variavel que ira receber o apertar da tecla "ESC"
+    const isEscKey = event.key === "Escape"        
+        if (isEscKey){
+            // thankyou fica invisivel (fecha)
+            thankyou_modal.style.display = "none";
+      
+            //Declarados para limpar os inputs
+            let fname_input = document.getElementById("subscribe_first_name");
+            let lname_input = document.getElementById("subscribe_last_name");
+            let mail_input = document.getElementById("subscribe_mail");
+            //limpando inputs
+            fname_input.value = '';
+            lname_input.value = '';
+            mail_input.value = '';    
+    }
+})
